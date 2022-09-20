2eb64c34bab06913a32a19c9abfad194e6401737
DirectX <DirectX@users.noreply.github.com>
2022-09-20 11:25:26 +0300

    Added rust-actix

--- rust-nickel/src/main.rs
+++ rust-nickel/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let mut server = Nickel::new();
 
     server.get("/", middleware!("Hello World!"));
-    server.get("/sayhello/:name", middleware! { |request|
-        format!("Hello, {:?}", request.param("name"))
+    server.get("/hello/:name", middleware! { |request|
+        format!("Hello {}", request.param("name").unwrap())
     });
 
     server.listen("127.0.0.1:5000").unwrap();
